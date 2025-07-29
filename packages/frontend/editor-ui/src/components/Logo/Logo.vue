2a422e689ee83d0fb60ddeb53747c0463ca94a1a
alfred896 <alfredocampoli96@gmail.com>
2025-07-29 17:51:22 +0200

    changes

--- packages/frontend/editor-ui/src/components/Logo/Logo.vue
+++ packages/frontend/editor-ui/src/components/Logo/Logo.vue
@@ -3,9 +3,9 @@ import type { FrontendSettings } from '@n8n/api-types';
 import { computed, onMounted, useCssModule, useTemplateRef } from 'vue';
 import { useFavicon } from '@vueuse/core';
 
-import LogoIcon from './logo-icon.svg';
-import LogoText from './logo-text.svg';
-
+//import LogoIcon from './logo-icon.svg';
+//import LogoText from './logo-text.svg';
+import LogoPura from './pura_logo.svg';
 const props = defineProps<
 	(
 		| {
@@ -22,10 +22,10 @@ const props = defineProps<
 
 const { location, releaseChannel } = props;
 
-const showLogoText = computed(() => {
-	if (location === 'authView') return true;
-	return !props.collapsed;
-});
+// const showLogoText = computed(() => {
+// 	if (location === 'authView') return true;
+// 	return !props.collapsed;
+// });
 
 const $style = useCssModule();
 const containerClasses = computed(() => {
@@ -46,8 +46,8 @@ onMounted(() => {
 	const logoEl = svg.value!.$el;
 
 	// Change the logo fill color inline, so that favicon can also use it
-	const logoColor = releaseChannel === 'dev' ? '#838383' : '#E9984B';
-	logoEl.querySelector('path')?.setAttribute('fill', logoColor);
+	// const logoColor = releaseChannel === 'dev' ? '#838383' : '#E9984B';
+	// logoEl.querySelector('path')?.setAttribute('fill', logoColor);
 
 	// Reuse the SVG as favicon
 	const blob = new Blob([logoEl.outerHTML], { type: 'image/svg+xml' });
@@ -57,8 +57,7 @@ onMounted(() => {
 
 <template>
 	<div :class="containerClasses" data-test-id="n8n-logo">
-		<LogoIcon ref="logo" :class="$style.logo" />
-		<LogoText v-if="showLogoText" :class="$style.logoText" />
+		<LogoPura ref="logo" :class="$style.logo" />
 		<slot />
 	</div>
 </template>
@@ -84,7 +83,8 @@ onMounted(() => {
 
 .logo,
 .logoText {
-	transform: scale(1.3) translateY(-2px);
+	// transform: scale(1.3) translateY(-2px);
+	transform: scale(0.7);
 }
 
 .logoText {
@@ -97,7 +97,7 @@ onMounted(() => {
 }
 
 .sidebarCollapsed .logo {
-	width: 40px;
+	width: 30px;
 	height: 30px;
 	padding: 0 var(--spacing-4xs);
 }
